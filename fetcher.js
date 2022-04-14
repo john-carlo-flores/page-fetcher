a3e490cb79eb1af517f0bf84da67ad49343ab0ee
JC Flores <tothenextcode@gmail.com>
2022-04-14 04:38:07 +0000

    Refactored code to work with prompt instead of readline

--- fetcher.js
+++ fetcher.js
@@ -1,41 +1,34 @@
- const request = require('request');
- const fs = require('fs');
- const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const request = require('request');
+const fs = require('fs');
+const prompt = require('prompt-sync')();
   
-const fetchResource = (url, path) => {
+const downloadURL = (url, path) => {
   const filePath = path.endsWith('/') ? path + 'index.html' : path + '/index.html';
-
-  if (path.char)
-
 
   if (!fs.existsSync(path)) {
     return console.log(`Error: File path provided is invalid.`);
-  } else if (fs.existsSync(filePath)) {
-    rl.question("File already exists on the path given. Would you like to overwrite it? (Y/N) ", (answer) => {
-      if (answer !== 'Y') {
-        rl.close();
-        return process.exit();
-      }
+  } 
 
-      rl.close();
-    });
+  if (fs.existsSync(filePath)) {
+    const answer = prompt("File already exists on the path given. Would you like to overwrite it? (Y/N) ");
+    if (answer !== 'Y') return process.exit();
   }
 
   request(url, (error, response, body) => {
-    if (error || (response >= 200 && response < 300)) {
-      return console.log(`Error: URL provided is invalid. File not downloaded.`);
+    if (error) {
+      return console.log(`Error: URL provided is invalid\n. ${error}.`);
     }
 
-    
+    if (response < 200 || response >= 300) {
+      return console.log(console.log('statusCode:', response && response.statusCode));
+    }
 
+    fs.writeFile(filePath, body, err => {
+      if (err) return console.log(err);
+    });
 
-    console.log('Downloaded and saved 1235 bytes to ./index.html.');
+    console.log(`Downloaded and saved ${body.length} bytes to ${filePath}.`)
   });
+};
 
-
-}
+downloadURL(process.argv[2], process.argv[3]);
